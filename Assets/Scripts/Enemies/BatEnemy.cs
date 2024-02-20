c55aa8aa459fb1eae50a248abc33106527a29009
ZombusterFTW <79545029+ZombusterFTW@users.noreply.github.com>
2024-02-19 23:12:07 -0500

    Initial upload of new scripts.
    Will attempt to have these last 2 completed before our meeting tomorrow if not shortly after :thumbsup:

---
+++ Assets/Scripts/Enemies/BatEnemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatEnemy : MonoBehaviour
+{
+    //Dies in about 2 hits. Flys and pursues Big Joe from a set distance. Damages Joe if ran into, can be damaged by jumping on its head, groundpouding, or other attacks. Takes 3 hits to dispatch with no weapon.
+    //Can shoot projectiles if Joe is within its firing range.
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Enemies/SpiderCat.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderCat : MonoBehaviour
+{
+    //Simple ambush AI. Hides on ceiling and drops down when Big Joe is in range. Lands and then begins to pursue Joe. Easily dispatched but quick
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
